223f810288f5f6f0bfca12b252e236e0bf9f97cc
uikoo9 <uikoo9@qq.com>
2022-03-14 21:42:23 +0800

    feat: add qiao-webpack

---
+++ webpack/qiao.webpack.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, '../src/index.js'),
+  },
+  plugins: [
+    {
+      type    : 'html',
+      title   : 'react guides',
+      inject  : 'body',
+      template: path.resolve(__dirname, './template.html')
+    }
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ["@babel/preset-react"] }
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
+  },
+};
